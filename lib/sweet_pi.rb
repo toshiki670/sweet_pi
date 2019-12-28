85fca6efc7b5e3813abf98a3416836f6d9408f07
Toshiki <toshiki.k.000@gmail.com>
2019-12-28 10:29:32 +0900

    Wip

--- lib/sweet_pi.rb
+++ lib/sweet_pi.rb
@@ -1,12 +1,48 @@
-require "sweet_pi/version"
-require "sweet_pi/runner"
-require "thor"
+# frozen_string_literal: true
+require 'sweet_pi/version'
+require 'sweet_pi/runner'
+require 'thor'
+
+# 実験用にRequireしているだけ
+require 'sweet_pi/model/chudnovsky'
+require 'sweet_pi/model/math'
+require 'benchmark'
+
 
 module SweetPi
   class << self
 
+    def test1(options = {})
+      @data = data
+      p data
+    end
 
+    def test3
+      x = 10
+      y = 10
 
+      r1 = Benchmark.realtime do
+        x.times do
+          SweetPi::Chudnovsky.single(y)
+        end
+      end
+
+      r2 = Benchmark.realtime do
+        x.times do
+          SweetPi::Chudnovsky.single2(y)
+        end
+      end
+
+      r3 = Benchmark.realtime do
+        x.times do
+          SweetPi::Chudnovsky.single3(y)
+        end
+      end
+
+      p r1
+      p r2
+      p r3
+    end
 
   end
 end
