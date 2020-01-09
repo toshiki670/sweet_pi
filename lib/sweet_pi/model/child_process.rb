9cd0531394813ef3e1195a3bcf114c825a125d09
Toshiki <toshiki.k.000@gmail.com>
2020-01-09 15:14:37 +0900

    Add ChildProcess

---
+++ lib/sweet_pi/model/child_process.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module SweetPi
+  class ChildProcess
+
+    def initialize(*argv, &block)
+      @status_read, @status_write = IO.pipe
+      Marshal.dump(nil, @status_write)
+      @value_read, @value_write = IO.pipe
+      Marshal.dump(nil, @value_write)
+
+      child_fork(*argv, &block)
+    end
+
+    def value
+      Process.waitpid @pid
+      Marshal.load(value_read)
+    end
+
+    private
+
+    def child_fork(*argv, &block)
+      @pid = Process.fork do
+        @value_read.close
+
+        status = :run
+        Marshal.dump(status, @status_write)
+
+        result = block.call(argv)
+
+        status = false
+      rescue
+        status = nil
+        result = nil
+      ensure
+        Marshal.dump(status, @status_write)
+        Marshal.dump(result, @value_write)
+      end
+    end
+
+  end
+end
