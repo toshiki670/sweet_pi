244f38544b0e386b0b022dfb2faeb0bfa3f5433f
Toshiki <toshiki.k.000@gmail.com>
2020-01-11 19:45:34 +0900

    Remove 前回の階乗の計算結果を用いて、階乗する方式

--- lib/sweet_pi/model/chudnovsky.rb
+++ lib/sweet_pi/model/chudnovsky.rb
@@ -3,7 +3,6 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 require 'sweet_pi/model/math'
 require 'sweet_pi/model/child_process'
-require 'sweet_pi/model/prev_factorial'
 
 module SweetPi
   class Chudnovsky
@@ -17,20 +16,6 @@ module SweetPi
     using SweetPi::Math::Factorial
 
 
-    def single_process2(digit)
-      accuracy = calc_accuracy(digit)
-
-      fact_1 = SweetPi::PrevFactorial.new
-      fact_3 = SweetPi::PrevFactorial.new
-      fact_6 = SweetPi::PrevFactorial.new
-      sum = SweetPi::Math.sum(0, accuracy) do |k|
-        Rational(numerator2(k, fact_6), denominator2(k, fact_1, fact_3))
-      end
-
-      result = '1.0'.to_d / (12 * sum)
-      fix(digit, result)
-    end
-
     def single_process(digit)
       accuracy = calc_accuracy(digit)
 
@@ -39,20 +24,6 @@ module SweetPi
       end
 
       result = '1.0'.to_d / (12 * sum)
-      fix(digit, result)
-    end
-
-    def multi_process2(digit, process_size)
-      accuracy = calc_accuracy(digit)
-
-      processes = []
-      process_size.times do |p_n|
-        processes << SweetPi::ChildProcess.new(accuracy, process_size, p_n) do |a, p_s, p_n|
-          each_process2(a, p_s, p_n)
-        end
-      end
-
-      result = '1.0'.to_d / processes.map(&:value).reduce(:+)
       fix(digit, result)
     end
 
@@ -71,31 +42,6 @@ module SweetPi
     end
 
     private
-
-    def each_process2(accuracy, process_size, process_num)
-      fact_1 = SweetPi::PrevFactorial.new
-      fact_3 = SweetPi::PrevFactorial.new
-      fact_6 = SweetPi::PrevFactorial.new
-
-      base = accuracy / process_size
-      mod = accuracy % process_size
-
-      first = base * process_num
-      last = base * (process_num + 1) - 1
-
-      if process_num < mod
-        first += process_num
-        last += process_num + 1
-      else
-        first += mod
-        last += mod
-      end
-
-      sum = SweetPi::Math.sum(first, last) do |k|
-        Rational(numerator2(k, fact_6), denominator2(k, fact_1, fact_3))
-      end
-      12 * sum
-    end
 
     def each_process(accuracy, process_size, process_num)
       f = Proc.new do |x|
@@ -120,14 +66,6 @@ module SweetPi
       (digit / DIGIT_PER_ACCURACY).ceil.to_i
     end
 
-    def numerator2(k, fact_6)
-      (-1)**k * fact_6.calc(6 * k) * (A + B * k)
-    end
-
-    def denominator2(k, fact_1, fact_3)
-      fact_1.calc(k)**3 * fact_3.calc(3 * k) * C**(3 * k + '1.5'.to_d)
-    end
-
     def numerator(k)
       (-1)**k * (6 * k).! * (A + B * k)
     end
@@ -139,7 +77,5 @@ module SweetPi
     def fix(digit, pi)
       pi.floor(digit)
     end
-
-
   end
 end
