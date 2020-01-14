d47bb0ee94338ee72e65992ee8e5fe01c17c87e6
Toshiki <toshiki.k.000@gmail.com>
2020-01-14 23:00:37 +0900

    Divide Generator

--- lib/sweet_pi.rb
+++ lib/sweet_pi.rb
@@ -2,47 +2,18 @@
 require 'sweet_pi/version'
 require 'sweet_pi/runner'
 require 'sweet_pi/pi'
+require 'sweet_pi/generator'
 
 require 'bigdecimal'
 require 'bigdecimal/util'
 
 
 module SweetPi
-  class Generator
+  class << self
 
-    def initialize(process_size = 1)
-      raise ArgumentError unless process_size.is_a?(Integer) and 1 <= process_size
-      @pi = SweetPi::PI.new(process_size)
-
-      @from = 0
-      @to = 2
+    def generator(process_size)
+      SweetPi::Generator.new(process_size)
     end
 
-    def range(from = 0, to)
-      @from = from
-      @to = to
-      self
-    end
-
-    def calc
-      @value = @pi.calc(@to)
-      self
-    end
-
-    def each_char
-      self.calc.to_s.each_char do |c|
-        yield c
-      end
-
-      self
-    end
-
-    def to_s
-      from = @from + 1 if 1 <= @from
-      to = @to + 2
-
-      @value ||= Math::PI.to_d
-      @value.to_s('f')[from..to]
-    end
   end
 end
