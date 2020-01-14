b8c33aa01dd3df05a9ab2a7e3dd356e63224acf7
Toshiki <toshiki.k.000@gmail.com>
2020-01-14 23:36:20 +0900

    #8 Rename

--- lib/sweet_pi/runner.rb
+++ lib/sweet_pi/runner.rb
@@ -7,7 +7,7 @@ module SweetPi
     default_command :calc_pi
     desc "calc", "Calculate PI"
     option :digit, aliases: :d, type: :numeric, default: 2, desc: "Number of digits in decimal part of PI."
-    option :thread, aliases: :t, type: :numeric, default: 1, desc: "CPU threads."
+    option :process, aliases: :p, type: :numeric, default: 1, desc: "CPU processes."
     option :output, aliases: :o, type: :string, required: :true, desc: "Output destination."
     option :result, aliases: :r, type: :string, desc: "Output of Processing result. standard output if not setting."
     def calc_pi
